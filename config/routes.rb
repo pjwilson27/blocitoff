6f2d07436f2ec81d9a1bc7d1772cbf5686dda592
Phil Wilson <pjwilson27@gmail.com>
2017-04-17 14:42:20 +0000

    Working...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   get 'home/index'
 
   devise_for :users
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-
-RSpec.describe HomeController, type: :controller do
+RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
