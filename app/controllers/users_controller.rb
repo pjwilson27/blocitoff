29b9bcb01f11e305ca9da798d3fc1831e5bafe4a
Phil Wilson <pjwilson27@gmail.com>
2017-05-02 01:40:31 +0000

    Working v2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def index
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def edit
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+  end
 end
