a8bd6ed3ca8dbc312c5f19285b0da562dce5d5af
Phil Wilson <pjwilson27@gmail.com>
2017-05-07 22:59:03 +0000

    Finished CP5--styling needs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def create
+    @item = current_user.items.create(item_params)
+    if @item.save
+      redirect_to users_show_path
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
