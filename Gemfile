be4fe5bdff9f1d871969f06d291ae67d1b27c11a
Phil Wilson <pjwilson27@gmail.com>
2017-05-07 23:13:08 +0000

    Completed CP6

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.0'
   gem 'factory_girl_rails', '~> 4.0'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+    User.create!(
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(8)
+    )
+end
+
+50.times do
+    Item.create!(
+        name: Faker::Hipster.sentence(3)
+    )
+end
