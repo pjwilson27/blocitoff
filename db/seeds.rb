842bb861d86d2cd1746202be600466278d67b186
Phil Wilson <pjwilson27@gmail.com>
2017-05-08 22:58:27 +0000

    Still working...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,24 @@
-50.times do
+require 'faker'
+
+10.times do
     User.create!(
         email: Faker::Internet.email,
         password: Faker::Internet.password(8)
     )
 end
 
-50.times do
+    demo = User.create!(
+        email: 'blocster@bloc.io',
+        password: 'qwerty'
+    )
+    
+
+5.times do
     Item.create!(
         name: Faker::Hipster.sentence(3)
     )
 end
+
+puts "Seeding is complete."
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
