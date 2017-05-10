83db27b4f668a413601cc148ab4f1afb1abf58c5
Phil Wilson <pjwilson27@gmail.com>
2017-05-10 00:18:35 +0000

    Updated specs, fixed seeding, played a bit more with some displays

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,21 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  
-  #before :each do
-   # sign_in user
-  #end
-  
-  
-  let(:user_attributes) do
-    {
-      name: "Blocster",
-      email: "blocster@bloc.io",
-      password: "helloworld"
-    }
-  end
+let(:my_user) {create(:user) }
 
   describe "GET #show" do
+    before do
+      sign_in(:my_user)
+    end
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
