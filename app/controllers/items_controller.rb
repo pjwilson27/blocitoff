1aa5c20b8473a7982291f99c41bb3c6691ef4d51
Phil Wilson <pjwilson27@gmail.com>
2017-05-14 15:59:44 +0000

    Got stupid delete working finally

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,20 +3,21 @@ class ItemsController < ApplicationController
     @item = current_user.items.create(item_params)
     if @item.save
       flash[:notice] = "You just made the list, #{@item.name}!"
+      redirect_to home_index_path
     end
   end
   
   def destroy
     @item = Item.find(params[:id])
-    @user = User.find(params[:id])
+    @user = current_user.items.destroy
     
     
     if @item.destroy
       flash[:notice] = "You have successfully deleted '#{@item.name}'."
-      redirect_to user_path
+      redirect_to home_index_path
     else
       flash.now[:alert] = "Unable to process your request..Try again later."
-      redirect_to user_path
+      redirect_to home_index_path
     end
   end
   
