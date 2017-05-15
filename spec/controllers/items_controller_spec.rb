3666305579e0facc4d29f8a8cf06cb2051e42544
Phil Wilson <pjwilson27@gmail.com>
2017-05-15 23:36:50 +0000

    Added some styles, some TDD

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
   
   let(:my_user) {create(:user) }
-  let(:my_item) {create(:item, user: my_user) }
+  let(:my_item) {create(:item, user_id: my_user) }
 
   describe "GET #create" do
     it "returns http success" do
@@ -11,5 +11,17 @@ RSpec.describe ItemsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
+  
+  describe "DELETE destroy" do
+    it "redirects to the show" do
+      delete :destroy, :my_item
+      expect(response).to redirect_to(user_path(current_user))
+    end
+  end
+  
+  describe "DELETE destroy" do
+    it "deletes the item" do
+      delete :destroy, :my_item
+    end
+  end
 end
