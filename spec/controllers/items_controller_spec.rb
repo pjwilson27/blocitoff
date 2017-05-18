8d8084dab98ec8ebfb7e741384751ab70585bd02
Phil Wilson <pjwilson27@gmail.com>
2017-05-18 22:23:44 +0000

    Wrapping

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -6,22 +6,22 @@ RSpec.describe ItemsController, type: :controller do
   let(:my_item) {create(:item, user_id: my_user) }
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    it "redirects to show" do
+      post :create, name: "something", user: my_user
+      expect(response).to redirect_to(user_path(my_user))
     end
   end
   
   describe "DELETE destroy" do
     it "redirects to the show" do
-      delete :destroy, :my_item
-      expect(response).to redirect_to(user_path(current_user))
+      delete :destroy, my_item
+      expect(response).to redirect_to(user_path(my_user))
     end
   end
   
   describe "DELETE destroy" do
     it "deletes the item" do
-      delete :destroy, :my_item
+      delete :destroy, my_item
     end
   end
 end
